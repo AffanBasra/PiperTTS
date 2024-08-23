4b1539090ee1e95240ab119a549f2a510319d662
AffanBasra <159237605+AffanBasra@users.noreply.github.com>
2024-08-23 15:27:15 +0500

    Update main.py

--- main.py
+++ main.py
@@ -56,5 +56,5 @@ def shutdown():
     return {"message": "Shutting down"}
 #initialization
 if __name__ == '__main__':
-    uvicorn.run(app, port=8909, host='0.0.0.0')
+    uvicorn.run(app, port=8000, host='0.0.0.0')
     print('Success')
